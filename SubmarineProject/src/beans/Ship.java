bd41a04290f3f72b675f5a095c0d9ed8d0e873be
jafuli <58686091+jafuli@users.noreply.github.com>
2020-07-09 12:37:01 +0300

    Update Ship.java

--- SubmarineProject/src/beans/Ship.java
+++ SubmarineProject/src/beans/Ship.java
@@ -2,21 +2,22 @@ package beans;
 
 public class Ship {
 
-	private static int id = 0;
+	private static int id = 1;
 	private int visibleId;
 	private int length;
 	private boolean horizontal;
 	private boolean sunk;
 	private String model;
+	private String link;
 	private int hitsCounter = 0;
 
 	public Ship(int length, boolean horizontal) {
 		super();
-		++id;
 		this.visibleId = id;
 		this.length = length;
 		this.horizontal = horizontal;
 		setModel(length);
+		setLink(length);
 	}
 
 	public int getLength() {
@@ -59,12 +60,6 @@ public class Ship {
 
 	}
 
-	@Override
-	public String toString() {
-		return model + " [length=" + length + ", horizontal=" + horizontal + ", sunk=" + sunk + ", model=" + model
-				+ "]";
-	}
-
 	public int getHitsCounter() {
 		return hitsCounter;
 	}
@@ -72,7 +67,8 @@ public class Ship {
 	public void setHitsCounter() {
 		this.hitsCounter++;
 		if (this.hitsCounter == length) {
-			System.out.println(model + " sunk");
+			System.out.println(model + " sunk"); // ship sunk message for user
+			System.out.println("Would you like to know more? " + link);
 			setSunk(true);
 		}
 	}
@@ -83,6 +79,25 @@ public class Ship {
 
 	public int getVisibleId() {
 		return visibleId;
+	}
+
+	public static void setId(int id) {
+		Ship.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(int length) {
+		if (length == 2)
+			this.link = "https://13news.co.il/item/news/domestic/crime-law/crime/submarine-33866/?fbclid=IwAR29LvPYZYzEtYqRAP-OdtebmIPgHaP3HkUt-5F7CJDBkpRUtvuKCbXSSdM";
+		if (length == 3)
+			this.link = "https://www.maariv.co.il/journalists/Article-775234?fbclid=IwAR33xI9HE3OLU9RCfXqD_m5O_hB3UVsc4Tnbp6tiwpUaRbBR_5s4HgqTBHc";
+		if (length == 4)
+			this.link = "https://www.youtube.com/watch?v=EcRDd3XNl6M&t=1s&fbclid=IwAR1rrQeuGzp98VFMEYgbAJJG0Tr7FuUlUznhAWUNAYi8wEOWscoa-tX-q1E";
+		if (length == 5)
+			this.link = "https://www.youtube.com/watch?v=UMrJ44Ut-RY&feature=youtu.be&fbclid=IwAR2BeTlfktMqYNlae6A3itAVLVm_dZPqEo5CssZ_fUOgH2zjIwFZavKl0A4";
 	}
 
 }
