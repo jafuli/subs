df8b3b90e06311fe7c46f65955441446f939cfde
jafuli <58686091+jafuli@users.noreply.github.com>
2020-07-09 12:37:21 +0300

    Update Checks.java

--- SubmarineProject/src/inputCheck/Checks.java
+++ SubmarineProject/src/inputCheck/Checks.java
@@ -6,6 +6,7 @@ import beans.Ship;
 
 public class Checks {
 
+	// check input for ship placement and shot coordinates
 	public static boolean checkInputForInt(String input) {
 		if (input.length() == 1) {
 			for (int i = 0; i < 10; i++) {
@@ -20,17 +21,9 @@ public class Checks {
 		return false;
 	}
 
-	public static boolean checkInputForChar(char input) {
-		char inp = Character.toUpperCase(input);
-		for (int i = 65; i < 75; i++) {
-			if ((int) inp == i)
-				return true;
-		}
-		return false;
-	}
-
+	
 	public void checkShipPlacement(Player player, int indexR, int indexC, Ship ship) throws indexAlreadyTakenException {
-		int[][] testBoard = new int[10][10];
+		int[][] testBoard = new int[10][10]; // create test board for input and correct index tests
 		for (int i = 0; i < testBoard.length; i++) {
 			for (int j = 0; j < testBoard.length; j++) {
 				testBoard[i][j] = player.getPlayerBoard()[i][j];
@@ -39,22 +32,22 @@ public class Checks {
 		if (ship.isHorizontal()) {
 			for (int i = 0; i < ship.getLength(); indexC++, i++) {
 				if (testBoard[indexR][indexC] > 0) {
-					throw new indexAlreadyTakenException();
+					throw new indexAlreadyTakenException(); // check for no overlapping ship placements
 				} else {
 					try {
-						testBoard[indexR][indexC] = 1;
+						testBoard[indexR][indexC] = 1; // check for correct index of play board (0-9)
 					} catch (IndexOutOfBoundsException e) {
-						System.out.println("index out of bounds");
+						System.out.println("index out of bounds"); 
 					}
 				}
 			}
 		} else {
 			for (int i = 0; i < ship.getLength(); indexR++, i++) {
 				if (testBoard[indexR][indexC] > 0) {
-					throw new indexAlreadyTakenException();
+					throw new indexAlreadyTakenException(); // check for no overlapping ship placements
 				} else {
 					try {
-						testBoard[indexR][indexC] = 1;
+						testBoard[indexR][indexC] = 1; // check for correct index of play board (0-9)
 					} catch (IndexOutOfBoundsException e) {
 						System.out.println("index out of bounds");
 					}
